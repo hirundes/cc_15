b6bf9cf99aa17d68f0a8302156a33f517a3f0187
HIRUN <hirundesilva97@gmail.com>
2025-03-10 14:33:29 -0400

    Task 3 - Removing Risk Items

--- cc_15.js
+++ cc_15.js
@@ -8,11 +8,20 @@ function addRiskItem(riskName, riskLevel, department) {
     const riskCard = document.createElement("div");   
     riskCard.classList.add('riskCard');
 
+    //Added resolveButton for Task 3
+    const resolveButton = document.createElement("button"); 
+    resolveButton.textContent = "Resolve";
+    resolveButton.addEventListener("click", (event) => { 
+        event.stopPropagation();     
+    
+        riskDashboard.removeChild(riskCard); });
+
     //Added riskCard content
     riskCard.innerHTML = `  
         <h3>${riskName}</h3>
         <span>Risk Level: <strong>${riskLevel}</strong></span>
         <p>${department}</p>
+        <button class="resolve-btn">Resolve</button> 
     `;  
 
     //Appending to container
@@ -22,5 +31,22 @@ function addRiskItem(riskName, riskLevel, department) {
 //Test Cases
 addRiskItem("Data Breach", "High", "IT");
 addRiskItem("Supply Chain Disruption", "Medium", "Operations");
+
+//Task 3 - Removing Risk Items
+function removingRisk () {  //Function to be called to remove the card
+    document.getElementById("riskDashboard").addEventListener("click", function(event) {
+        if (event.target.classList.contains("resolve-btn")) {
+            event.stopPropagation();
+            event.target.parentElement.remove(); //Removes the card
+        }
+    });
+};
+//Calling removingRisk
+removingRisk();
+
+//Test Case
+addRiskItem("Market Fluctuations", "High", "Finance");
+
+
 
 
